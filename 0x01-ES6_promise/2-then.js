e45cfa19a5208bd4bed3a15b744d6271f5b80c6d
Elijahdanie <elijahdanie@gmail.com>
2022-04-09 14:23:59 +0100

    bug fixes

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -3,8 +3,8 @@ export default function handleResponseFromAPI(promise) {
     .then(() => {
       console.log('Got a response from the API');
       return {
-        status: 200,
         body: 'success',
+        status: 200,
       };
     })
     .catch(() => new Error())
