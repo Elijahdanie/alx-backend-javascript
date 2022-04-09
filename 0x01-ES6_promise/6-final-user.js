2d7f7a32e860309945ce29af70137a310d8c6916
Elijahdanie <elijahdanie@gmail.com>
2022-04-09 14:19:45 +0100

    made use of a seperate array

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,13 +5,19 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((results) => results.map((x) => (x.status === 'fulfilled'
-    ? {
-      status: x.status,
-      value: x.value,
-    }
-    : {
-      status: x.status,
-      value: `Error: ${x.reason}`,
-    })));
+  ]).then((results) => {
+    const payload = [];
+    results.forEach((x) => payload.push(
+      x.status === 'fulfilled'
+        ? {
+          status: x.status,
+          value: x.value,
+        }
+        : {
+          status: x.status,
+          value: `Error: ${x.reason}`,
+        },
+    ));
+    return payload;
+  });
 }
