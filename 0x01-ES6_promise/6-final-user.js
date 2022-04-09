2649e7a9701cae3abccd30b95e2c712a62cca356
Elijahdanie <elijahdanie@gmail.com>
2022-04-09 15:03:41 +0100

    last trial to fix problem with this number

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,22 +2,19 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((results) => {
-    const payload = [];
-    results.forEach((x) => payload.push(
-      x.status === 'fulfilled'
-        ? {
-          status: x.status,
-          value: x.value,
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((result) => {
+      const res = [];
+      result.forEach((p) => {
+        if (p.status === 'fulfilled') {
+          res.push(p);
+        } else {
+          res.push({
+            status: p.status,
+            p: `Error: ${p.reason.message}`,
+          });
         }
-        : {
-          status: x.status,
-          value: `Error: ${x.reason}`,
-        },
-    ));
-    return payload;
-  });
+      });
+      return res;
+    });
 }
