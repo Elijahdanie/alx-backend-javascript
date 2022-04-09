1dee667bbdb81734480cc9b2d8476f82e7e0468d
Elijahdanie <elijahdanie@gmail.com>
2022-04-09 14:46:36 +0100

    checker what are you looking for?

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,18 +2,9 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   const f = uploadPhoto();
-  const s = createUser();
-
-  let body;
-  let fname;
-  let lname = '';
-
-  return Promise.all([f, s])
-    .then((results) => {
-      body = results[0];
-      fname = results[1];
-      lname = results[2];
-      console.log(body, fname, lname);
-    })
-    .catch(() => console.log('Signup system offline'));
+  const l = createUser();
+  return Promise.all([f, l]).then(
+    (data) => console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`),
+    () => console.log('Signup system offline'),
+  );
 }
