32018d292f1aea9104ac6318ee36d8d041e23f13
Elijahdanie <elijahdanie@gmail.com>
2022-04-09 15:07:39 +0100

    fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,21 +2,18 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const allpromises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
-
-  return Promise.allSettled(allpromises)
-    .then((result) => {
-      const res = [];
-      result.forEach((p) => {
-        if (p.status === 'fulfilled') {
-          res.push(p);
-        } else {
-          res.push({
-            status: p.status,
-            p: `Error: ${p.reason.message}`,
-          });
-        }
-      });
-      return res;
-    });
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((results) => {
+    results.map((x) => (x.status === 'fulfilled'
+      ? {
+        status: 'fulfilled',
+        value: x.value,
+      }
+      : {
+        status: x.status,
+        value: `Error: ${x.reason}`,
+      }));
+  });
 }
