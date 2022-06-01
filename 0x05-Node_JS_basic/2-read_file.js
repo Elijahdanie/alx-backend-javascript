baddb3c107f99abbfae0722060b8ed9a37b06446
Elijahdanie <elijahdanie@gmail.com>
2022-06-01 01:16:35 +0100

    update

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  let file = '';
+  try {
+    file = fs.readFileSync(path);
+  } catch (err) {
+    // eslint-disable-next-line no-throw-literal
+    throw ('Cannot load the database');
+  }
+
+  const parsed = file.toString('utf-8').split('\n');
+  console.log(`Number of students: ${parsed.length - 1}`);
+  const fields = {};
+  const students = {};
+  parsed.forEach((x) => {
+    const data = x.split(',');
+    const arr = data[3].split('\r');
+    const [key] = arr;
+    if (key !== 'field') {
+      fields[key] = fields[key] !== undefined ? fields[key] + 1 : 1;
+      if (students[key] !== undefined) {
+        students[key] += (`${data[0]}, `);
+      } else {
+        students[key] = `${data[0]}, `;
+      }
+    }
+  });
+  Object.keys(fields).forEach((x) => {
+    console.log(`Number of students in ${x}: ${fields[x]}. List: ${students[x].slice(0, -2)}`);
+  });
+}
+
+module.exports = countStudents;
---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (err, file) => {
+      if (err) {
+        reject(Error('Cannot load the database'));
+      } else {
+        const parsed = file.toString('utf-8').split('\n');
+        console.log(`Number of students: ${parsed.length - 1}`);
+        const fields = {};
+        const students = {};
+        parsed.forEach((x) => {
+          const data = x.split(',');
+          const arr = data[3].split('\r');
+          const [key] = arr;
+          if (key !== 'field') {
+            fields[key] = fields[key] !== undefined ? fields[key] + 1 : 1;
+            if (students[key] !== undefined) {
+              students[key] += (`${data[0]}, `);
+            } else {
+              students[key] = `${data[0]}, `;
+            }
+          }
+        });
+        Object.keys(fields).forEach((x) => {
+          console.log(`Number of students in ${x}: ${fields[x]}. List: ${students[x].slice(0, -2)}`);
+        });
+        resolve();
+      }
+    });
+  });
+}
+
+module.exports = countStudents;
